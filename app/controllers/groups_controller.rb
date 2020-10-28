c3be3dca848e2ffb16140466933b09acb7d8733c
AIRETSEM <airetsem@gmail.com>
2020-10-28 22:04:06 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
@@ -39,6 +39,28 @@ class GroupsController < ApplicationController
   def destroy
     @group.destroy
     redirect_to groups_path, alert: "群組已刪除"
+  end
+
+  def join
+    @group = Group.find(params[:id])
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入討論版成功"
+    else
+      flash[:warning] = "你已經是本討論版成員了"
+    end
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本討論版"
+    else
+      flash[:warning] = "你又不是本討論版成員是要退屁喔w"
+    end
+    redirect_to group_path(@group)
   end
 
   private
